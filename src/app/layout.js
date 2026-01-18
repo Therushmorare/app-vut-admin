19742e87736df0ee650cb5971916c91992e0547f
Therushmorare <tshego.tt.morare@gmail.com>
2026-01-18 14:33:50 +0200

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,19 @@
-import "./styles/globals.css"
+// app/layout.js
+import "./styles/globals.css";
 import Layout from './components/Layout';
-
+import { usePathname } from 'next/navigation';
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
+  // Hide layout for login
+  const hideLayout = pathname.startsWith('/login');
+
   return (
     <html lang="en">
       <body>
-        <Layout>
-          {children}
-        </Layout>
+        {!hideLayout ? <Layout>{children}</Layout> : children}
       </body>
     </html>
   );
 }
-
