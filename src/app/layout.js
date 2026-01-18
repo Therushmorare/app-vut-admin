a52350d7ae99e437df3af5ab12ca3faeac7d65da
Therushmorare <tshego.tt.morare@gmail.com>
2026-01-18 14:57:50 +0200

    fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,16 @@
-// app/layout.js
-import "./styles/globals.css";
+import "./styles/globals.css"
 import Layout from './components/Layout';
-import { usePathname } from 'next/navigation';
+
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
-
-  // Hide layout for login
-  const hideLayout = pathname.startsWith('/login');
-
   return (
     <html lang="en">
       <body>
-        {!hideLayout ? <Layout>{children}</Layout> : children}
+        <Layout>
+          {children}
+        </Layout>
       </body>
     </html>
   );
 }
+
